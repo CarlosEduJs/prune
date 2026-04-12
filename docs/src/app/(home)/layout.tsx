5b516505ae78b021b71034a0e5290b8ba08b0faf
Carlos Edu Js <carloseduardogit@gmail.com>
2026-04-12 11:12:14 -0300

    docs(lp): add header

--- docs/src/app/(home)/layout.tsx
+++ docs/src/app/(home)/layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      hello world
-    </div>
+    <main className="flex h-screen w-screen flex-col gap-4 bg-linear-to-br from-primary/10 to-secondary/10 p-4">
+      {children}
+    </main>
   );
 }
--- docs/src/app/(home)/page.tsx
+++ docs/src/app/(home)/page.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
   return (
-    <div className="flex flex-col justify-center text-center flex-1">
-      <h1 className="text-2xl font-bold mb-4">Hello World</h1>
+    <div className="flex flex-col">
+      <header className="flex items-center justify-between px-12 py-6">
+        <h1 className="text-2xl font-bold">prune</h1>
+        <nav>
+          <ul className="flex gap-4 items-center">
+            <li><Link className="text-sm font-light" href="/">Home</Link></li>
+            <li><Link className="text-sm font-light" href="/docs">Docs</Link></li>
+          </ul>
+        </nav>
+        <Button variant={"outline"} size={"lg"}>
+          Get Started
+        </Button>
+      </header>
     </div>
   );
 }
