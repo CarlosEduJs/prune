c44a66fb846726937e2be1b4b6cab18107048c45
Carlos Edu Js <carloseduardogit@gmail.com>
2026-04-14 13:43:43 -0300

    chore: add examples

---
+++ examples/ts-alias/src/index.ts
@@ -0,0 +1,5 @@
+import { foo } from "@/utils/helper";
+import { bar } from "~/components/Button";
+import { local } from "./local";
+
+console.log(foo, bar, local);
---
+++ examples/ts-alias/src/utils/helper.ts
@@ -0,0 +1,5 @@
+export const foo = "foo";
+
+export function unusedHelper() {
+    return "unused";
+}
