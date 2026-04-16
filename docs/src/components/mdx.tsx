e5e84fba6a79c73b7e865248cc4be1c7e3e5ccc1
Carlos Edu Js <carloseduardogit@gmail.com>
2026-04-16 11:52:51 -0300

    Add mermaid diagram

--- docs/src/components/mdx.tsx
+++ docs/src/components/mdx.tsx
@@ -1,11 +1,13 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import * as LucideIcons from 'lucide-react';
+import { Mermaid } from './mdx/mermaid';
 import type { MDXComponents } from 'mdx/types';
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
     ...(LucideIcons as unknown as MDXComponents),
+    Mermaid,
     ...components,
   } satisfies MDXComponents;
 }
